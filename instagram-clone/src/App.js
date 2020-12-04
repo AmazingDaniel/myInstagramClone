fd04129f6cc5d2ae6161ba04dd7befcc9e4e53f8
Daniel Moreno <drmjr417@sbcglobal.net>
2020-12-04 05:30:29 -0600

    created Post.css & import in Post.js & added classNames for post

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Post from './Post'
+import Post from './Post';
 
 function App() {
   return (
--- instagram-clone/src/Post.js
+++ instagram-clone/src/Post.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import './Post.css'
 
 function Post() {
     return (
-        <div>
+        <div className="post">
             <h3>Username</h3>
             {/* header -> avatar + username */}
 
+            <img className="postImage" src="https://files.hiv.gov/s3fs-public/instagramlogoresized-1.jpg" />
             {/* image */}
 
+            <h4>Username: caption</h4>
             {/* username + caption */}
         </div>
     )
