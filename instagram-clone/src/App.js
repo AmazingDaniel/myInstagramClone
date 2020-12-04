59f1112fd9a1cb87e234406dd7204b11f1076ea5
Daniel Moreno <drmjr417@sbcglobal.net>
2020-12-04 14:48:43 -0600

    created modal and button signUp

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -1,27 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
-import { db } from './firebase'
+import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyle = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyle();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
-    
-// useEffect Runs a piece of code base on a specific condition
+  const [open, setOpen] = useState(false);
 
-useEffect(() => {
-  // this where the code runs
-  db.collection('posts').onSnapshot(snapshot => {
-    // everytime a new post is added, this code fires...
-    setPosts(snapshot.docs.map(doc => ({
-      id: doc.id, 
-      post: doc.data()
-    })));
-  })
-}, []);
+  useEffect(() => {
+    // this where the code runs
+    db.collection('posts').onSnapshot(snapshot => {
+      // everytime a new post is added, this code fires...
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
+    })
+  }, []);
 
+  const signUp = (event) => {
+
+  }
 
   return (
     <div className="App">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>Text in a modal</h2>
+        </div>
+      </Modal>
+
       <div className="appHeader">
         <img
           className="appHeaderImage"
@@ -29,12 +69,14 @@ useEffect(() => {
           alt=""
         />
       </div>
-      
+
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+
       <h1>Programming Instagram-Clone</h1>
 
       {
-        posts.map(({id, post}) => (
-          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}  />
+        posts.map(({ id, post }) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
 
